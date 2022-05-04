205be90a63659a2651240b6f63c39ea7977bf452
janomaciell <janomaciel1@gmail.com>
2022-05-04 20:23:10 -0300

    Homework JS I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -66,12 +66,11 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x === y);
-  return true
-} 
-return false
-
- 
+   if (x === y);{
+   return true
+   } 
+   return false;
+  } 
   
 
 
@@ -79,25 +78,23 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
- if (str1 === str2);
- return false;
+   if (str1 === str2); {
+   return false;
+   }
+ return true
 }
-  return true;
-
-
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
- if (num >90);
- return true
-
-} 
-   if (num <90);
-     return false;
-
-
+   if (num >90);{
+   return true
+   }
+   if (num <90);{
+   return false;
+   }
+  }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
@@ -119,24 +116,25 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 0);{
-  return true; 
+   if (num % 2 === 0);{
+   return true; 
    } 
-   else if (num % 2 === 1); 
+    if (num % 2 !== 0);{ 
      return false;
-   
+    }
+  }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 1)
+  if (num % 2 !== 0);{
   return true;
    } 
-   else (num % 2 === 0) 
+   if (num % 2 === 0) {
      return false
-  
-
+  }
+}
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
@@ -185,12 +183,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero>0);
-     return 'es positivo'  
-} else if (numero<0);{
+  if (numero>0); {
+     return 'es positivo'
+  }  
+   if (numero<0);{
     return "es negarivo";
-} else if (numero===0) { 
+  }  if (numero===0) { 
   return false;
+  }
 }
 
 
@@ -254,10 +254,21 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if ( letra == a,e,i,o,u);
-   return "es vocal :)"
-} 
-return "dato incorrecto :("
+  if (letra.lenght > 1) {
+   return "dato incorrecto"
+   }
+   if ( 
+   letra === "a" ||
+   letra === "e" ||
+   letra === "i" ||
+   letra === "o" ||
+   letra === "u" 
+   )  {
+   return "es vocal" ;}
+ 
+
+   return "dato incorrecto"; 
+}   
 
 
 
